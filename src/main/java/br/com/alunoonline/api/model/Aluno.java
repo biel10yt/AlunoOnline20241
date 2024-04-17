1d123f905e6533be9e60a733684345e3f45d5390
biel10yt <Gabrielsoares25022005@gmail.com>
2024-04-16 21:24:24 -0300

    Atualização

--- src/main/java/br/com/alunoonline/api/model/Aluno.java
+++ src/main/java/br/com/alunoonline/api/model/Aluno.java
@@ -1,6 +1,9 @@
 package br.com.alunoonline.api.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +16,10 @@ import java.io.Serializable;
 @Data
 @Entity
 public class Aluno implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+    private String email;
 
 }
